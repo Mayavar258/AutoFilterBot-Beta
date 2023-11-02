b045703468dc393266ec2a8f60b4e10eea2954c1
Mayavar258 <123068420+Mayavar258@users.noreply.github.com>
2023-11-02 19:56:03 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -8,12 +8,13 @@ logging.getLogger("pyrogram").setLevel(logging.ERROR)
 logging.getLogger("cinemagoer").setLevel(logging.ERROR)
 
 from pyrogram import Client, __version__
+import pyromod.listen
 from pyrogram.raw.all import layer
 from database.ia_filterdb import Media
 from aiohttp import web
 from database.users_chats_db import db
 from web import web_server
-from info import SESSION_STRING, LOG_CHANNEL, API_ID, API_HASH, BOT_TOKEN, PORT, BIN_CHANNEL
+from info import LOG_CHANNEL, API_ID, API_HASH, BOT_TOKEN, PORT, BIN_CHANNEL
 from utils import temp
 from typing import Union, Optional, AsyncGenerator
 from pyrogram import types
@@ -38,26 +39,7 @@ class Bot(Client):
         b_users, b_chats = await db.get_banned()
         temp.BANNED_USERS = b_users
         temp.BANNED_CHATS = b_chats
-        try:
-            await super().start()
-        except (AccessTokenExpired, AccessTokenInvalid):
-            logging.error("Your BOT_TOKEN revoke and add again, exiting now")
-            exit()
-        if len(SESSION_STRING) != 0:
-            user_bot = Client(
-                name='Auto_Filter_User_Bot',
-                api_id=API_ID,
-                api_hash=API_HASH,
-                session_string=SESSION_STRING
-            )
-            try:
-                await user_bot.start()
-                name = f'@{username}' if (username := user_bot.me.username) else user_bot.me.first_name
-                logging.info(f'User Bot [{name}] Started!')
-                temp.USER_BOT = user_bot
-            except:
-                logging.error("Your SESSION_STRING revoke and add again, exiting now")
-                exit()
+        await super().start()
         if os.path.exists('restart.txt'):
             with open("restart.txt") as file:
                 chat_id, msg_id = map(int, file)
