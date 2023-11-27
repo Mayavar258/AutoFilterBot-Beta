e14cf3155e116d2f46cf66b967c385f85c09a3da
Mayavar258 <123068420+Mayavar258@users.noreply.github.com>
2023-11-27 14:49:40 +0530

    Update info.py

--- info.py
+++ info.py
@@ -27,7 +27,7 @@ BOT_TOKEN = environ.get('BOT_TOKEN', '6804702516:AAHnNXwMoIF3EnibTAI7frU-MZwv4pd
 if len(BOT_TOKEN) == 0:
     logging.error('BOT_TOKEN is missing, exiting now')
     exit()
-SESSION_STRING = environ.get('SESSION_STRING', 'movie search')
+SESSION_STRING = environ.get('SESSION_STRING', '')
 PORT = int(environ.get('PORT', '8080'))
 
 # Bot pics
